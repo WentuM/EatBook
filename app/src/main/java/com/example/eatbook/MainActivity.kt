74462cb7c24532fa6fdeb15065e079b9cd8a5f17
WentuM <kesand@mail.ru>
2021-03-19 16:53:10 +0300

    debug nav controller

--- app/src/main/java/com/example/eatbook/MainActivity.kt
+++ app/src/main/java/com/example/eatbook/MainActivity.kt
@@ -1,85 +1,20 @@
 package com.example.eatbook
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.fragment_profile.*
-import kotlinx.android.synthetic.main.toolbar.*
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_profile)
-        var imgProfile = findViewById<ImageView>(R.id.profile_image)
-        imgProfile.setImageResource(R.drawable.prifile)
-//        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-//
-//        val navController = findNavController(R.id.nav_host_fragment)
-//        // Passing each menu ID as a set of Ids because each
-//        // menu should be considered as top level destinations.
-//        val appBarConfiguration = AppBarConfiguration(setOf(
-//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//        navView.setupWithNavController(navController)
-        btn_profile_book.setOnClickListener {
-            Toast.makeText(this, "sdsa", Toast.LENGTH_LONG).show()
-        }
-        txv_profile_city.text = "Kazan"
-        txv_profile_email.text = "kesand@mail.ru"
-        txv_profile_number.text = "89993332211"
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        imgv_profile_edit.setOnClickListener {
-            imgv_profile_edit.visibility = View.INVISIBLE
-            imgv_profile_active.visibility = View.VISIBLE
-            imgv_profile_cancel.visibility = View.VISIBLE
-            enableProfileInfo()
-        }
-
-        imgv_profile_cancel.setOnClickListener {
-            imgv_profile_edit.visibility = View.VISIBLE
-            imgv_profile_active.visibility = View.INVISIBLE
-            imgv_profile_cancel.visibility = View.INVISIBLE
-            disableProfileInfo()
-        }
-
-        imgv_profile_active.setOnClickListener {
-            edit(txv_profile_fullname, edtx_profile_fullname)
-            edit(txv_profile_city, edtx_profile_city)
-            edit(txv_profile_number, edtx_profile_number)
-            edit(txv_profile_email, edtx_profile_email)
-            enableProfileInfo()
-            Toast.makeText(this, "Изменения сохранены", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun enableProfileInfo() {
-        txv_profile_number.visibility = View.INVISIBLE
-        txv_profile_email.visibility = View.INVISIBLE
-        txv_profile_city.visibility = View.INVISIBLE
-        edtx_profile_email.visibility = View.VISIBLE
-        edtx_profile_number.visibility = View.VISIBLE
-        edtx_profile_city.visibility = View.VISIBLE
-    }
-
-    private fun disableProfileInfo() {
-        txv_profile_number.visibility = View.VISIBLE
-        txv_profile_email.visibility = View.VISIBLE
-        txv_profile_city.visibility = View.VISIBLE
-        edtx_profile_email.visibility = View.INVISIBLE
-        edtx_profile_number.visibility = View.INVISIBLE
-        edtx_profile_city.visibility = View.INVISIBLE
-    }
-
-    private fun edit(etView: TextView, etEdit: EditText) {
-        etView.text = etEdit.text.toString()
-        etView.visibility = View.VISIBLE
-        etEdit.visibility = View.INVISIBLE
+        val navController = findNavController(R.id.nav_host_fragment)
+        navView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/example/eatbook/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/eatbook/ui/dashboard/DashboardFragment.kt
@@ -1,31 +1,113 @@
-//package com.example.eatbook.ui.dashboard
-//
-//import android.os.Bundle
-//import android.view.LayoutInflater
-//import android.view.View
-//import android.view.ViewGroup
-//import android.widget.TextView
-//import androidx.fragment.app.Fragment
-//import androidx.lifecycle.Observer
-//import androidx.lifecycle.ViewModelProviders
-//import com.example.eatbook.R
-//
-//class DashboardFragment : Fragment() {
-//
-//    private lateinit var dashboardViewModel: DashboardViewModel
-//
-//    override fun onCreateView(
-//            inflater: LayoutInflater,
-//            container: ViewGroup?,
-//            savedInstanceState: Bundle?
-//    ): View? {
-//        dashboardViewModel =
-//                ViewModelProviders.of(this).get(DashboardViewModel::class.java)
-//        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-//        val textView: TextView = root.findViewById(R.id.text_dashboard)
-//        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-//            textView.text = it
-//        })
-//        return root
-//    }
-//}
+package com.example.eatbook.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.eatbook.R
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.android.synthetic.main.toolbar.*
+
+
+class DashboardFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: DashboardViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+            ViewModelProviders.of(this).get(DashboardViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            txv_profile_city.text = it
+        })
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initFields()
+        initClickListener()
+    }
+
+    private fun initFields() {
+        profile_image.setImageResource(R.drawable.prifile)
+        txv_profile_city.text = "Kazan"
+        txv_profile_email.text = "kesand@mail.ru"
+        txv_profile_number.text = "89993332211"
+    }
+
+    private fun initClickListener() {
+        btn_profile_book.setOnClickListener {
+            Snackbar.make(
+                requireActivity().findViewById(android.R.id.content),
+                "qweqw",
+                Snackbar.LENGTH_LONG
+            ).show()
+        }
+
+        imgv_profile_edit.setOnClickListener {
+            imgv_profile_edit.visibility = View.INVISIBLE
+            imgv_profile_active.visibility = View.VISIBLE
+            imgv_profile_cancel.visibility = View.VISIBLE
+            enableProfileInfo()
+        }
+
+        imgv_profile_cancel.setOnClickListener {
+            imgv_profile_edit.visibility = View.VISIBLE
+            imgv_profile_active.visibility = View.INVISIBLE
+            imgv_profile_cancel.visibility = View.INVISIBLE
+            disableProfileInfo()
+        }
+
+        imgv_profile_active.setOnClickListener {
+            edit(txv_profile_fullname, edtx_profile_fullname)
+            edit(txv_profile_city, edtx_profile_city)
+            edit(txv_profile_number, edtx_profile_number)
+            edit(txv_profile_email, edtx_profile_email)
+            enableProfileInfo()
+            Snackbar.make(
+                requireActivity().findViewById(android.R.id.content),
+                "Изменения сохранены",
+                Snackbar.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private fun enableProfileInfo() {
+        txv_profile_number.visibility = View.INVISIBLE
+        txv_profile_email.visibility = View.INVISIBLE
+        txv_profile_city.visibility = View.INVISIBLE
+        edtx_profile_email.visibility = View.VISIBLE
+        edtx_profile_number.visibility = View.VISIBLE
+        edtx_profile_city.visibility = View.VISIBLE
+    }
+
+    private fun disableProfileInfo() {
+        txv_profile_number.visibility = View.VISIBLE
+        txv_profile_email.visibility = View.VISIBLE
+        txv_profile_city.visibility = View.VISIBLE
+        edtx_profile_email.visibility = View.INVISIBLE
+        edtx_profile_number.visibility = View.INVISIBLE
+        edtx_profile_city.visibility = View.INVISIBLE
+    }
+
+    private fun edit(etView: TextView, etEdit: EditText) {
+        etView.text = etEdit.text.toString()
+        etView.visibility = View.VISIBLE
+        etEdit.visibility = View.INVISIBLE
+    }
+}
